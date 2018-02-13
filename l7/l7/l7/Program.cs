96b7bc959513e8c55edd4499c6aa3954cb32fe6a
sleter <sleter97@gmail.com>
2018-02-13 16:29:37 +0100

    l7 - interfaces

---
+++ l7/l7/l7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle buick = new Vehicle("Buick", 4, 160);
+
+            if(buick is IDrivable)
+            {
+                buick.Move();
+                buick.Stop();
+            }
+            else
+            {
+                Console.WriteLine("The {0} can't be driven", buick.Brand);
+            }
+
+            IElectronicDevice TV = TVRemote.GetDevice();
+            PowerButton powBut = new PowerButton(TV);
+            powBut.Execute();
+            powBut.Undo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ l7/l7/l7/Television.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l7
+{
+    class Television : IElectronicDevice
+    {
+        public int Volume { get; set; }
+
+
+        public void Off()
+        {
+            Console.WriteLine("TV is off");
+        }
+
+        public void On()
+        {
+            Console.WriteLine("TV is on");
+        }
+
+        public void VolumeDown()
+        {
+            if (Volume != 0) Volume--;
+            Console.WriteLine($"TV volume is at {Volume}");
+        }
+
+        public void VolumeUp()
+        {
+            if (Volume != 100) Volume++;
+            Console.WriteLine($"TV volume is at {Volume}");
+        }
+    }
+}
